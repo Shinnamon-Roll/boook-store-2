03a6857128282152896258efce95773c3793f08b
EqrixZ <s6606022630049@email.kmutnb.ac.th>
2024-10-16 21:11:44 +0700

    done_test.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { Sequelize, DataTypes } from 'sequelize';
 const app = express();
 
 // Database Connection
-const sequelize = new Sequelize('mydatabase', 'Shinnamon', '123456789', {
+const sequelize = new Sequelize('mydatabase', 'eqq', 'az123456', {
   host: 'localhost',
   dialect: 'postgres',
 });
@@ -38,109 +38,109 @@ const BookStore = sequelize.define('BookStore', {
   timestamps: false
 });
 
-// // Books Model
-// const Books = sequelize.define('Books', {
-//   BookID: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   BookName: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   },
-//   BookTypeID: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false
-//   },
-//   BookPrice: {
-//     type: DataTypes.FLOAT,
-//     allowNull: false
-//   },
-//   Description: {
-//     type: DataTypes.TEXT,
-//     allowNull: true
-//   }
-// }, {
-//   tableName: 'Books',
-//   timestamps: false
-// });
+// Books Model
+const Books = sequelize.define('Books', {
+  BookID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  BookName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  BookTypeID: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  BookPrice: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  Description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  }
+}, {
+  tableName: 'Books',
+  timestamps: false
+});
 
-// // Customers Model
-// const Customers = sequelize.define('Customers', {
-//   CustomerID: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   MemberID: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false
-//   },
-//   CustomerName: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   },
-//   Sex: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   },
-//   Age: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false
-//   },
-//   Contact: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false
-//   },
-//   Address: {
-//     type: DataTypes.TEXT,
-//     allowNull: false
-//   }
-// }, {
-//   tableName: 'Customers',
-//   timestamps: false
-// });
+// Customers Model
+const Customers = sequelize.define('Customers', {
+  CustomerID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  MemberID: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  CustomerName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  Sex: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  Age: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  Contact: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  Address: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  }
+}, {
+  tableName: 'Customers',
+  timestamps: false
+});
 
-// // Members Model
-// const Members = sequelize.define('Members', {
-//   MemberID: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   Point: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false
-//   },
-//   JoinDate: {
-//     type: DataTypes.DATE,
-//     allowNull: false
-//   },
-//   MembershipLevel: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   }
-// }, {
-//   tableName: 'Members',
-//   timestamps: false
-// });
+// Members Model
+const Members = sequelize.define('Members', {
+  MemberID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  Point: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  JoinDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  MembershipLevel: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: 'Members',
+  timestamps: false
+});
 
-// // BookTypes Model
-// const BookTypes = sequelize.define('BookTypes', {
-//   BookTypeID: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true
-//   },
-//   BookTypeName: {
-//     type: DataTypes.STRING,
-//     allowNull: false
-//   }
-// }, {
-//   tableName: 'BookTypes',
-//   timestamps: false
-// });
+// BookTypes Model
+const BookTypes = sequelize.define('BookTypes', {
+  BookTypeID: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  BookTypeName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: 'BookTypes',
+  timestamps: false
+});
 
 
 
@@ -162,4 +162,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-export {BookStore};
+export {BookStore,Members,Books,Customers,BookTypes};
